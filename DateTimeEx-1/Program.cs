f89f9fc1a52e7dceff8ef47ef7334c966151dcbb
Reddipalli <899167@cognizant.com>
2021-04-28 20:10:57 +0530

    Add project files.

---
+++ DateTimeEx-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeEx_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime r;
+            Console.Write("Enter Your Name: ");
+            string s = Console.ReadLine();
+            Console.Write("Enter Your Date Of Birth(mm/dd/yyyy hh:mm:ss): ");
+            string dob = Console.ReadLine();
+            r = DateTime.Parse(dob, System.Globalization.CultureInfo.InvariantCulture);
+            DateTime today = DateTime.Now;
+            Console.WriteLine("Your day of Birthday is : " + r.DayOfWeek);
+            Console.WriteLine("Your date of Birthday in Indian Format: " + r.ToString("dd-MM-yyyy"));
+            Console.WriteLine("No of days since your birthday: " + (DateTime.Now - r).Days);
+            DateTime next = r.AddYears(today.Year - r.Year);
+            if (next < today)
+                next = next.AddYears(1);
+            int numDays = (next - today).Days;
+            Console.WriteLine("No of days to celebrate your next birthday : " + numDays);
+            Console.ReadKey();
+        }
+    }
+}
